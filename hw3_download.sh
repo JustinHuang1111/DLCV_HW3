a4923221f3a2ea0e8595d6d3543f8ec332f3adb7
JustinHuang1111 <73697339+JustinHuang1111@users.noreply.github.com>
2022-11-24 17:36:09 +0800

    update download

--- hw3_download.sh
+++ hw3_download.sh
@@ -1,3 +1,4 @@
 python -c "import clip; clip.load('ViT-B/32')"
 gdown --fuzzy https://drive.google.com/file/d/1ml5mlpwmSukmMQbC3vnFfQXcOR7_58Tm/view?usp=sharing -O ./p2/caption_tokenizer.json
-gdown --fuzzy https://drive.google.com/file/d/1Dnz2zBIHDrXOtCKdHs2Fyil6TwWuCj0T/view?usp=sharing -O ./p2/p2.pth
+# gdown --fuzzy https://drive.google.com/file/d/1Dnz2zBIHDrXOtCKdHs2Fyil6TwWuCj0T/view?usp=sharing -O ./p2/p2.pth
+wget https://www.dropbox.com/s/asfl0cqvzir6siy/1117-0215_epsilon_50_best.pth?dl=1 -O ./p2/p2.pth
